7239ad4f3f167914704068b5a7720c27e8852689
Jose Jimenez <josejimenez826@gmail.com>
2021-02-18 23:27:55 -0800

    fixed a function in script.js, but still needs work

--- script.js
+++ script.js
@@ -10,38 +10,118 @@ function writePassword() {
 
 }
 
+//Functions is working now need to make uppercase, the min and max for password, and look into why i get undifined every now and then.
+
 function generatePassword(){
-  var pass = ""
-  var pro = prompt("How many letters do you want. (8 min, 128 max)")
-  var pro1 = confirm("Do you want special chaacters. ($, %, &)")
-  var pro2 = confirm("Do you want lowercase letters.")
-  var pro3 = confirm("Do you want uppercase letters.")
-  var pro4 = confirm("Do you want numbers.")
+  var p = prompts();
+  var password = "";
 
   var arr1 = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","}","|","~"]
   var arr2 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
   var arr3 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
   var arr = [];
+  var a;
+  // console.log(p)
+  // console.log(p[1])
 
-  if (pro1){
-    arr.concat(arr1)
+  if (p[1] === true){
+    // arr = [].concat(arr1)
+    // console.log(arr)
+    // console.log(arr.length)
+
+    if (arr.length == 0){
+      arr = [].concat(arr1)
+      // console.log(arr)
+      // console.log("1")
+    }
+    else{
+      arr = arr.concat(arr1)
+      // console.log(arr)
+    }
   }
-  if (pro2){
-    arr.concat(arr2)
+
+  if (p[2] === true){
+    // arr = [].concat(arr1)
+    // console.log(arr)
+    // console.log(arr.length)
+
+    if (arr.length == 0){
+      arr = [].concat(arr2)
+      // console.log(arr)
+      // console.log("1")
+    }
+    else{
+      arr = arr.concat(arr2)
+      // console.log(arr)
+    }
   }
+
+  // if (p[3] === true){
+  //   // arr = [].concat(arr1)
+  //   // console.log(arr)
+  //   // console.log(arr.length)
+
+  //   if (arr.length != 0){
+  //     a = arr.concat(arr3)
+  //     // console.log(a)
+  //   }
+  //   else{
+  //     a = [].concat(arr3)
+  //     // console.log(a)
+  //   }
+  // }
+
+  if (p[3] === true){
+    // arr = [].concat(arr1)
+    // console.log(arr)
+    // console.log(arr.length)
+
+    if (arr.length != 0){
+      arr = arr.concat(arr3)
+      // console.log(arr)
+    }
+    else{
+      arr = [].concat(arr3)
+      // console.log(arr)
+    }
+  }
+
+  // if (p[2]){
+  //   console.log(arr)
+  //   a = arr.concat(arr2)
+  //   console.log(a)
+  // }
   // if (pro3){
   //   arr.concat(arr2)
   // }
-  if (pro4){
+  if (p[4]){
     arr.concat(arr3)
   }
 
-  for(var i = 0; i < pro; i++){// need to fix this so it can returned to click. 
-    pass.concat(arr[Math.random(arr.length)])
+  var i = 0;
+  while(i < p[0]){
+    var num = Math.floor(Math.random() * arr.length) + 1
+    password += arr[num]
+    // console.log(a[num])
+    // console.log(a.length)
+    i++
   }
 
-  return pass
+  console.log(password)
+  return password
+}
+
+function prompts(){
+  var pro = prompt("How many letters do you want. (8 min, 128 max)")
+  var pro1 = confirm("Do you want special chaacters. ($, %, &)")
+  var pro2 = confirm("Do you want lowercase letters.")
+  var pro3 = confirm("Do you want uppercase letters.")
+  var pro4 = confirm("Do you want numbers.")
+
+  var p = [pro, pro1, pro2, pro3, pro4]
+
+  return p
 }
 
 // Add event listener to generate button
