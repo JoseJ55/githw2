cae65786e550b6910ef6d769be6083d509dc2859
Jose Jimenez <josejimenez826@gmail.com>
2021-02-21 10:35:21 -0800

    fixed the reason for getting undefined and made it possible for uppercase

--- script.js
+++ script.js
@@ -10,114 +10,70 @@ function writePassword() {
 
 }
 
-//Functions is working now need to make uppercase, the min and max for password, and look into why i get undifined every now and then.
-
+//Functions is working now needs the min and max for password.
 function generatePassword(){
   var p = prompts();
   var password = "";
 
-  var arr1 = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","}","|","~"]
-  var arr2 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  var arr3 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
+  var arr1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var arr2 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var arr3 = ["1","2","3","4","5","6","7","8","9","0"]
+  var arr4 = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","}","|","~"]
   var arr = [];
-  var a;
-  // console.log(p)
-  // console.log(p[1])
 
-  if (p[1] === true){
-    // arr = [].concat(arr1)
-    // console.log(arr)
-    // console.log(arr.length)
-
+  if (p[1] === true){ // part for lowercase letters
     if (arr.length == 0){
       arr = [].concat(arr1)
-      // console.log(arr)
-      // console.log("1")
     }
     else{
       arr = arr.concat(arr1)
-      // console.log(arr)
     }
   }
 
-  if (p[2] === true){
-    // arr = [].concat(arr1)
-    // console.log(arr)
-    // console.log(arr.length)
-
+  if (p[2] === true){ // part for uppercase letters
     if (arr.length == 0){
       arr = [].concat(arr2)
-      // console.log(arr)
-      // console.log("1")
     }
     else{
       arr = arr.concat(arr2)
-      // console.log(arr)
     }
   }
 
-  // if (p[3] === true){
-  //   // arr = [].concat(arr1)
-  //   // console.log(arr)
-  //   // console.log(arr.length)
-
-  //   if (arr.length != 0){
-  //     a = arr.concat(arr3)
-  //     // console.log(a)
-  //   }
-  //   else{
-  //     a = [].concat(arr3)
-  //     // console.log(a)
-  //   }
-  // }
-
-  if (p[3] === true){
-    // arr = [].concat(arr1)
-    // console.log(arr)
-    // console.log(arr.length)
-
-    if (arr.length != 0){
-      arr = arr.concat(arr3)
-      // console.log(arr)
+  if (p[3] === true){ // part for numbers 
+    if (arr.length == 0){
+      arr = [].concat(arr3)
     }
     else{
-      arr = [].concat(arr3)
-      // console.log(arr)
+      arr = arr.concat(arr3)
     }
   }
 
-  // if (p[2]){
-  //   console.log(arr)
-  //   a = arr.concat(arr2)
-  //   console.log(a)
-  // }
-  // if (pro3){
-  //   arr.concat(arr2)
-  // }
-  if (p[4]){
-    arr.concat(arr3)
+  if (p[4] === true){ // part for special chacters
+    if (arr.length == 0){
+      arr = [].concat(arr4)
+    }
+    else{
+      arr = arr.concat(arr4)
+    }
   }
 
+  // this is to add to random characters to the password variable
   var i = 0;
   while(i < p[0]){
-    var num = Math.floor(Math.random() * arr.length) + 1
+    var num = Math.floor(Math.random() * arr.length)
     password += arr[num]
-    // console.log(a[num])
-    // console.log(a.length)
     i++
   }
 
-  console.log(password)
   return password
 }
 
 function prompts(){
   var pro = prompt("How many letters do you want. (8 min, 128 max)")
-  var pro1 = confirm("Do you want special chaacters. ($, %, &)")
-  var pro2 = confirm("Do you want lowercase letters.")
-  var pro3 = confirm("Do you want uppercase letters.")
-  var pro4 = confirm("Do you want numbers.")
+  var pro1 = confirm("Do you want lowercase letters.")
+  var pro2 = confirm("Do you want uppercase letters.")
+  var pro3 = confirm("Do you want numbers.")
+  var pro4 = confirm("Do you want special charcters. ($, %, &)")
 
   var p = [pro, pro1, pro2, pro3, pro4]
 
